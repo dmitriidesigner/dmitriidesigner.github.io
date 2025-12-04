359a5020844338f6ab199184d1fa94e8ee8e7ee4
dmitriidesigner <dmitriikrasnoshapka@gmail.com>
2025-12-05 02:36:26 +0400

    Build fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   outDir: 'docs',
+  build: {
+    assets: 'assets',
+    inlineStylesheets: 'never'
+  },
   vite: {
     plugins: [tailwindcss()]
   }
